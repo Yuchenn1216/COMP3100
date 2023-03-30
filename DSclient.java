d17c951b545851ec73543066ece00cee91dab678
Yuchenn1216 <105789655+Yuchenn1216@users.noreply.github.com>
2023-03-30 14:49:07 +1100

    Stage1

---
+++ DSclient.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.net.*;
+
+public class DSclient {
+	public static void main(String[] args) {
+
+		try {
+			// create a socket
+			Socket s = new Socket("localhost", 50000);
+			// Initialise input and output streams associated with the socket
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+			// System.out.println("Target IP: " + s.getInetAddress() + "Target Port: " +
+			// s.getPort());
+			// System.out.println("Local IP: " + s.getLocalAddress() + "Local Port: " +
+			// s.getLocalPort());
+
+			// connect ds-server
+			// Send HELO
+			out.write(("HELO\n").getBytes());
+			out.flush();
+			// System.out.println("sent HELO");
+			String str = (String) in.readLine();
+			// System.out.println("RCVD: " + str);
+
+			// Send AUTH username
+			String username = System.getProperty("user.name");
+			out.write(("AUTH " + username + "\n").getBytes());
+			out.flush();
+			// System.out.println("sent AUTH");
+			str = (String) in.readLine();
+			// System.out.println("RCVD: " + str);
+
+			// create variables for seaching largest server type and id
+			int largestCore = 0; // what's for?
+			int count = 0;
+			String largestSerType = "";
+			// String largestSerID = "";
+			Boolean flag = true;
+			int send = 0;
+			// While the last message from ds-server is not NONE do
+			while (true) {
+
+				out.write(("REDY\n").getBytes());
+				out.flush();
+				// System.out.println("sent REDY");
+				str = in.readLine();
+				// System.out.println("RCVD: " + str); // job message
+
+				if (str.equals("NONE"))
+					break;
+
+				// store the type of job and its id
+				String[] jobInfo = str.split(" ");
+				String jobType = jobInfo[0];
+				String jobID = jobInfo[2];
+				// System.out.println("jobType: " + jobType);
+				// System.out.println("jobID: " + jobID);
+
+				// if JCPL continue;
+				if (jobType.equals("JCPL"))
+					continue;
+
+				if (flag) { // using flag do it only once
+					out.write(("GETS All\n").getBytes());
+					out.flush();
+					// System.out.println("sent GETS All");
+					str = (String) in.readLine();
+					// System.out.println("RCVD: " + str); // DATA X Y
+
+					out.write(("OK\n").getBytes());
+					out.flush();
+					// System.out.println("sent OK");
+
+					String[] serverInfoList = str.split(" ");
+					int nRecs = Integer.parseInt(serverInfoList[1]); // get the number of servers
+					// System.out.println("nRecs: " + nRecs);
+
+					for (int i = 0; i < nRecs; i++) {
+						str = (String) in.readLine();
+						// System.out.println("RCVD: " + str);
+
+						// find the largest server type and id
+						String[] serverInfo = str.split(" ");
+						String serverType = serverInfo[0]; // type
+						// String serverID = serverInfo[1];
+						int coreNum = Integer.parseInt(serverInfo[4]); // core number
+
+						if (coreNum > largestCore) {
+							largestSerType = serverType;
+							// largestSerID = serverID;
+							largestCore = coreNum;
+							count = 1;
+						} else if (serverType.equals(largestSerType)) {
+							count++;
+						}
+
+					}
+
+					out.write(("OK\n").getBytes());
+					out.flush();
+					// System.out.println("sent OK");
+					str = (String) in.readLine();
+					// System.out.println("RCVD: " + str); // Received . indicating no more server
+					// info
+
+				}
+				flag = false;
+
+				// System.out.println("Largest Server Type: " + largestSerType);
+				// System.out.println("Largest Server CoreNumber: " + largestCore);
+
+				// schedule the job if it's JOBN
+				if (jobType.equals("JOBN")) {
+					String schdMsg = "SCHD " + jobID + " " + largestSerType + " " + send + "\n";
+					out.write(schdMsg.getBytes());
+					out.flush();
+					send++;
+					send = send % count;
+					// System.out.println("sent SCHD: " + schdMsg);
+					str = in.readLine();
+					// System.out.println("RCVD: " + str);
+				}
+			}
+
+			out.write(("QUIT\n").getBytes());
+			out.flush();
+			// System.out.println("sent QUIT");
+			str = in.readLine();
+			// System.out.println("RCVD: " + str);
+
+			in.close();
+			out.close();
+			s.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
+
