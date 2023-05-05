825c5dc6ff2ca1992be19cec27977b350dcbe675
Yuchenn1216 <105789655+Yuchenn1216@users.noreply.github.com>
2023-05-05 12:53:31 +1000

    Update week8workshop.java

--- week8workshop.java
+++ week8workshop.java
@@ -15,19 +15,18 @@ public class dsClient {
             // Send HELO
             out.write(("HELO\n").getBytes());
             out.flush();
-            System.out.println("sent HELO");
+            // System.out.println("sent HELO");
             String str = (String) in.readLine();
-            System.out.println("RCVD: " + str);
+            // System.out.println("RCVD: " + str);
 
             // Send AUTH username
             String username = System.getProperty("user.name");
             out.write(("AUTH " + username + "\n").getBytes());
             out.flush();
-            System.out.println("sent AUTH");
+            // System.out.println("sent AUTH");
             str = (String) in.readLine();
-            System.out.println("RCVD: " + str);
+            // System.out.println("RCVD: " + str);
 
-            Boolean flag = true;
             String[] serverInfo;
             String serverType = "";
             String serverID = "";
@@ -36,9 +35,10 @@ public class dsClient {
             while (true) {
                 out.write(("REDY\n").getBytes());
                 out.flush();
-                System.out.println("sent REDY");
+                // System.out.println("sent REDY");
                 str = in.readLine();
-                System.out.println("RCVD: " + str); // one of the following type: JOBN,JCPL orNone
+                // System.out.println("RCVD: " + str); // one of the following type: JOBN,JCPL
+                // orNone
 
                 if (str.equals("NONE"))
                     break;
@@ -51,46 +51,55 @@ public class dsClient {
                 if (jobType.equals("JCPL"))
                     continue;
 
-                if (flag) {
-                    String jobCore = jobInfo[4];
-                    String jobRam = jobInfo[5];
-                    String jobDisk = jobInfo[6];
+                String jobCore = jobInfo[4];
+                String jobRam = jobInfo[5];
+                String jobDisk = jobInfo[6];
 
-                    out.write(("GETS Capable" + " " + jobCore + " " + jobRam + " " + jobDisk + "\n").getBytes());
-                    out.flush();
-                    str = (String) in.readLine(); // DATA X Y
+                out.write(("GETS Capable" + " " + jobCore + " " + jobRam + " " + jobDisk + "\n").getBytes());
+                out.flush();
+                // System.out.println("sent GETS Capable");
+                str = (String) in.readLine(); // DATA X Y
+                // System.out.println("RCVD: " + str);
 
-                    out.write(("OK\n").getBytes());
-                    out.flush();
-                    String[] serverInfoList = str.split(" ");
-                    int nRecs = Integer.parseInt(serverInfoList[1]); // get the number of servers
+                out.write(("OK\n").getBytes());
+                out.flush();
+                // System.out.println("sent OK");
 
-                    // get the first server in the list and schedule the job to it
+                String[] serverInfoList = str.split(" ");
+                int nRecs = Integer.parseInt(serverInfoList[1]); // get the number of servers
+
+                // get the first server in the list and schedule the job to it
+                str = (String) in.readLine();
+                // System.out.println("RCVD: " + str);
+                serverInfo = str.split(" ");
+                serverType = serverInfo[0];
+                serverID = serverInfo[1];
+
+                for (int i = 1; i < nRecs; i++) {
                     str = (String) in.readLine();
-                    serverInfo = str.split(" ");
-                    serverType = serverInfo[0];
-                    serverID = serverInfo[1];
-
-                    for (int i = 1; i < nRecs; i++) {
-                        str = (String) in.readLine();
-                        System.out.println("RCVD: " + str);
-                    }
+                    // System.out.println("RCVD: " + str);
                 }
-                flag = false;
+                out.write(("OK\n").getBytes());
+                out.flush();
+                // System.out.println("sent OK");
+                str = (String) in.readLine(); // .
+                // System.out.println("RCVD: " + str);
 
                 // schedule the job if it's JOBN
                 if (jobType.equals("JOBN")) {
                     String schdMsg = "SCHD " + jobID + " " + serverType + " " + serverID + "\n";
                     out.write(schdMsg.getBytes());
                     out.flush();
+                    str = (String) in.readLine();
+                    // System.out.println("RCVD: " + str);
                 }
             }
 
             out.write(("QUIT\n").getBytes());
             out.flush();
-            System.out.println("sent QUIT");
+            // System.out.println("sent QUIT");
             str = in.readLine();
-            System.out.println("RCVD: " + str);
+            // System.out.println("RCVD: " + str);
             in.close();
             out.close();
             s.close();
